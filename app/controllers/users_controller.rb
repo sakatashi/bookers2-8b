4cafe8eecb3edbbb9e99f0da77273c116acae33c
sakatashi <shiori.1002sakata@gmail.com>
2022-09-25 07:20:25 +0000

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController
+  before_action :ensure_correct_user, only: [:update, :edit]
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+    @today_book = @books.created_today
+    @yesterday_book = @books.created_yesterday
+      @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+  end
+
+  def edit
+  end
+
+  def following
+    user = User.find(params[:id])
+    @users = user.following
+  end
+
+  def follower
+    user = User.find(params[:id])
+    @users = user.follower
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+end
+
---
+++ app/models/book.rb
@@ -0,0 +1,28 @@
+class Book < ApplicationRecord
+  belongs_to :user
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :view_counts, dependent: :destroy
+  validates :title,presence:true
+  validates :body,presence:true,length:{maximum:200}
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } # 今日
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } # 前日
+    scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } #今週
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } # 前週
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @book = Book.where("title LIKE?","#{word}")
+    elsif search == "forward_match"
+      @book = Book.where("title LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @book = Book.where("title LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @book = Book.where("title LIKE?","%#{word}%")
+    else
+      @book = Book.all
+    end
+  end
+end
